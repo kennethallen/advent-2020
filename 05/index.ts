0a3f0efaa3a4ddd56a2f132b7dde2c651a76b608
Kenneth Allen <kenneth@mostly.me>
2020-12-06 17:29:55 +1100

    Solve day 5

---
+++ 05/index.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt')).slice(undefined, -1).toString()
+const seatIds = data.split(/\r?\n/).map(s => parseInt(s.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2))
+console.log('Data loaded')
+
+seatIds.sort((a, b) => a - b)
+console.log('Data sorted')
+
+console.log(`Part 1: Max seating ID ${seatIds[seatIds.length - 1]}`)
+
+for (let i = 1; i < seatIds.length; i++) {
+  if (seatIds[i] - 1 !== seatIds[i - 1]) {
+    console.log(`Part 2: Discontinuity found ${seatIds.slice(i - 1, i + 1)}`)
+  }
+}
