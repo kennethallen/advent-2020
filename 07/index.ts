dbc5e09477d1e67fba6b2dca5a98092f18c0414c
Kenneth Allen <kenneth.a@onqsoft.com.au>
2020-12-07 16:52:29 +1100

    Solve day 7

---
+++ 07/index.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+class BagSpec {
+  contains = new Map<string, number>()
+  containedIn = new Set<string>()
+}
+const bags = new Map<string, BagSpec>()
+function getOrMake(color: string) {
+  let spec = bags.get(color)
+  if (!spec) {
+    spec = new BagSpec()
+    bags.set(color, spec)
+  }
+  return spec
+}
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt')).slice(undefined, -1).toString()
+const rules = data.split(/\r?\n/)
+
+rules.forEach(r => {
+  const color = r.match(/^(.*) bags contain/)![1]
+  const spec = getOrMake(color)
+  for (const [, count, otherColor] of r.matchAll(/(\d+) (.*?) bags?/g)) {
+    spec.contains.set(otherColor, parseInt(count))
+    getOrMake(otherColor).containedIn.add(color)
+  }
+})
+console.log('Data loaded')
+
+const toCheck = ['shiny gold']
+const parents = new Set<string>()
+while (toCheck.length) {
+  getOrMake(toCheck.pop()!).containedIn.forEach(pc => {
+    if (!parents.has(pc)) {
+      parents.add(pc)
+      toCheck.push(pc)
+    }
+  })
+}
+console.log(`Part 1: count possible parent bags of shiny gold ${parents.size}`)
+
+function countInclusive(color: string): number {
+  const spec = getOrMake(color)
+  return 1 + [...spec.contains.entries()].map(([c, n]) => n * countInclusive(c)).reduce((a, b) => a + b, 0)
+}
+console.log(`Part 2: count inner bags of shiny gold ${countInclusive('shiny gold') - 1}`)
