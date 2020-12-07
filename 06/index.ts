a48a00492fba355b07db6fbb739b9da90bdef8b9
Kenneth Allen <kenneth.a@onqsoft.com.au>
2020-12-07 17:27:10 +1100

    Do correct sum and intersection with lodash

--- 06/index.ts
+++ 06/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import sum from 'lodash'
+import { sum, intersection } from 'lodash'
 
 const data = fs.readFileSync(path.join(__dirname, 'input.txt')).slice(undefined, -1).toString()
 const groups = data.split(/(?:\r?\n){2}/)
@@ -9,5 +9,5 @@ console.log('Data loaded')
 const unionGroups = groups.map(s => new Set(s.match(/[a-z]/g)))
 console.log(`Part 1: Total yesses (groups unioned) ${sum(unionGroups.map(g => g.size))}`)
 
-const intersectGroups = groups.map(s => s.split(/\r?\n/).map(p => [...p]).reduce((a, b) => a.filter(q => b.includes(q))))
+const intersectGroups = groups.map(s => intersection(...s.split(/\r?\n/).map(p => [...p])))
 console.log(`Part 1: Total yesses (groups intersected) ${sum(intersectGroups.map(g => g.length))}`)
